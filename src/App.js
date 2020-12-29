7ee606f63b17a7d303ba86835e017cfb04686b86
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-29 15:49:14 -0500

    Change id to number instead of strings

--- src/App.js
+++ src/App.js
@@ -6,17 +6,17 @@ import Card from "./components/js/Card.js";
 function App() {
   const cards = [
     {
-      id: "0",
+      id: 0,
       backgroundColor: "#3db10a",
       text: "Liskov Substituion Principle",
     },
     {
-      id: "1",
+      id: 1,
       backgroundColor: "blue",
       text: "Interface Segregation Principle",
     },
     {
-      id: "2",
+      id: 2,
       backgroundColor: "yellow",
       text: "Dependency Inversion",
     },
