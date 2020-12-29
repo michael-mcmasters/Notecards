4c01befde6bbd226c9615a749b4e00d94a767c33
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-29 18:08:25 -0500

    Use different method to have cards flip when mouse hovers

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -1,8 +1,21 @@
-import react from "react";
+import React from "react";
 import "../css/Card.css";
 
 export default function Card(props) {
   return (
-    <h1 className="card" style={{ backgroundColor: props.backgroundColor }}>{props.text}</h1>
+    <div>
+      <div className="flip-card">
+        <div className="flip-card-inner" style={{ backgroundColor: props.backgroundColor }}>
+          <div className="flip-card-front" style={{ backgroundColor: props.backgroundColor }}>
+            <h1>{props.text}</h1>
+          </div>
+          <div className="flip-card-back" style={{ backgroundColor: props.backgroundColor }}>
+            <h1>John Doe</h1>
+            <p>Architect and Engineer</p>
+            <p>We love that guy</p>
+          </div>
+        </div>
+      </div>
+    </div>
   );
 }
