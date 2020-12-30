650e8a550d91501c37ca57a0a88bc80885eed05f
michael-mcmasters <jmcmasters411@gmail.com>
2020-12-29 21:11:07 -0500

    Cards now flip when clicked but have no transition

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -1,24 +1,32 @@
-import React, { useEffect, useRef } from "react";
+import React, { useEffect, useState } from "react";
 import "../css/Card.css";
 
 export default function Card(props) {
+  const [flipped, setFlipped] = useState(false);
+
+  const getFlipDirection = () => {
+    if (flipped === true) {
+      return { transform: "rotateY(180deg)" };
+    } else {
+      return { transform: "rotateY(0deg)" };
+    }
+  }
+
   const handleKeyDown = (event) => {
     console.log('A key was pressed', event.keyCode);
   };
 
-  // Only runs when component mounts because we pass zero dependencies []
-  useEffect(() => {
+  useEffect(() => {   // Only runs when component mounts because we pass zero dependencies []
     window.addEventListener('keydown', handleKeyDown);
 
-    // This arrow function is called when the component dismounts.
-    return () => {
+    return () => {    // This arrow function is called when the component dismounts.
       window.removeEventListener('keydown', handleKeyDown);
     };
   }, []);
 
   return (
     <div>
-      <div className="flip-card">
+      <div className="flip-card" onClick={() => setFlipped(!flipped)} style={getFlipDirection()}>
         <div className="flip-card-inner" style={{ backgroundColor: props.backgroundColor }}>
           <div className="flip-card-front" style={{ backgroundColor: props.backgroundColor }}>
             <h1>{props.text}</h1>
