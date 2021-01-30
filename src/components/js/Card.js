f41f44da55ca696e31babfc726a408675b29eaab
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-30 14:35:01 -0500

    Change card into a styled component

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -1,8 +1,54 @@
 import React, { useEffect, useState } from "react";
+import styled, { css } from "styled-components";
 import "../css/Card.css";
 
 export default function Card(props) {
   const [flipped, setFlipped] = useState(false);
+
+  const CardContainer = styled.div`
+    width: 250px;
+    height: 320px;
+    background: none;
+    margin: 2rem;
+    cursor: pointer;
+    position: relative;
+    transition: left 0.8s;
+  `;
+
+  const Card = styled.div`
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    transform-style: preserve-3d;
+    transition: all 0.8s ease;
+  `;
+
+  const Front = styled.div`
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    backface-visibility: hidden;
+    -webkit-backface-visibility: hidden;
+    overflow: hidden;
+    
+    background-color: ${props.backgroundColor};
+  `;
+  const Back = styled.div`
+    position: absolute;
+    top: 0;
+    left: 0;
+    width: 100%;
+    height: 100%;
+    border-radius: 10px;
+    backface-visibility: hidden;
+    overflow: hidden;
+    background: #fafafa;
+    color: #333;
+    text-align: center;
+    transform: rotateY(180deg);
+  `;
 
   const handleKeyDown = (event) => {
     if (event.key === "ArrowRight") {
@@ -36,17 +82,34 @@ export default function Card(props) {
   }
 
   return (
-    <div >
-      <div class="card-container">
-        <div class="card" onClick={() => handleCardFlip()}>
-          <div class="front" style={{ background: props.backgroundColor }}>
+    // <div>
+    //   <div className="card-container">
+    //     <div className="card" onClick={() => handleCardFlip()}>
+    //       <div className="front" style={{ background: props.backgroundColor }}>
+    //         <h1>{props.frontText}</h1>
+    //         <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p>
+    //       </div>
+    //       <div className="back">
+    //         <p>{props.backText}</p>
+    //         <button>Submit</button>
+    //       </div>
+    //     </div>
+    //   </div>
+    // </div>
+
+    <div>
+      <CardContainer>
+        <Card onClick={() => handleCardFlip()}>
+          <Front>
             <h1>{props.frontText}</h1>
-            <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p></div>
-          <div class="back">
+            <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p>
+          </Front>
+          <Back>
             <p>{props.backText}</p>
-            <button>Submit</button></div>
-        </div>
-      </div>
+            <button>Submit</button>
+          </Back>
+        </Card>
+      </CardContainer>
     </div>
   );
 };
