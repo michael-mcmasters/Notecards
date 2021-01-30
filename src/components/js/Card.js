ac14acc46f061b82b09e3a26106adfd3f72ded7d
michael-mcmasters <jmcmasters411@gmail.com>
2021-01-30 13:50:00 -0500

    Arrow keys now move card. Space key flips it

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -5,13 +5,12 @@ export default function Card(props) {
   const [flipped, setFlipped] = useState(false);
 
   const handleKeyDown = (event) => {
-    // console.log('A key was pressed', event.keyCode);
     if (event.key === "ArrowRight") {
-      console.log("Right");
+      moveCard("left")
     } else if (event.key === "ArrowLeft") {
-      console.log("Left");
+      moveCard("right");
     } else if (event.key === " ") { // space key
-      flipCard();
+      handleCardFlip();
     }
   };
 
@@ -21,20 +20,25 @@ export default function Card(props) {
     return () => {    // This arrow function is called when the component dismounts.
       window.removeEventListener('keydown', handleKeyDown);
     };
-  }, [flipped]); // Rerender component whenever this value changes.
+  }, [flipped]); // Re-renders component whenever flipped value changes.
 
   // transform-style: preserve-3d was not working by assigning with React no matter what I tried. So here I am directly manipulating the CSS file.
   // This makes the cards look more 3D as they flip.
-  const flipCard = () => {
+  const handleCardFlip = () => {
     const elements = document.querySelectorAll(".card");
     elements[props.index].style.transform = flipped ? "rotateY(0deg)" : "rotateY(180deg)";
     setFlipped(!flipped);
   }
 
+  const moveCard = (direction) => {
+    const elements = document.querySelectorAll(".card-container");
+    elements[props.index].style.left = (direction === "left") ? "300px" : "-300px";
+  }
+
   return (
     <div >
       <div class="card-container">
-        <div class="card" onClick={() => flipCard()}>
+        <div class="card" onClick={() => handleCardFlip()}>
           <div class="front" style={{ background: props.backgroundColor }}>
             <h1>{props.frontText}</h1>
             <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p></div>
