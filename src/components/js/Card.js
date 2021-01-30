81a061aa8e7ea58e2c409261f1037005fc77bd29
Michael McMasters <michaelmcmasters@Michaels-Air.fios-router.home>
2021-01-29 22:51:41 -0500

    Add more styling and props

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -19,29 +19,20 @@ export default function Card(props) {
   // transform-style: preserve-3d was not working by assigning with React no matter what I tried. So here I am directly manipulating the CSS file.
   // This makes the cards look more 3D as they flip.
   const flipCard = () => {
-    const elements = document.querySelectorAll(".flip-card-inner");
+    const elements = document.querySelectorAll(".card");
     elements[props.index].style.transform = flipped ? "rotateY(0deg)" : "rotateY(180deg)";
     setFlipped(!flipped);
   }
 
   return (
-    <div>
-
-      <div className="maincontainer" onClick={() => flipCard()}>
-        <div className="thecard" style={{ backgroundColor: props.backgroundColor }}>
-          <div className="thefront" style={{ backgroundColor: props.backgroundColor }}>
+    <div >
+      <div class="card-container">
+        <div class="card" onClick={() => flipCard()}>
+          <div class="front" style={{ background: props.backgroundColor }}>
             <h1>{props.frontText}</h1>
-          </div>
-          <div className="theback" style={{ backgroundColor: props.backgroundColor }}>
+            <p>This is the front of the card. It contains important information. Please see overleaf for more details.</p></div>
+          <div class="back">
             <p>{props.backText}</p>
-          </div>
-        </div>
-      </div>
-
-      <div class="maincontainer">
-        <div class="thecard">
-          <div class="thefront"><h1>Front of Card</h1><p>This is the front of the card. It contains important information. Please see overleaf for more details.</p></div>
-          <div class="theback"><h1>Back of Card</h1><p>{props.backText}</p>
             <button>Submit</button></div>
         </div>
       </div>
