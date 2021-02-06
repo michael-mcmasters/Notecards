7fd9596c904e92386b8327e219259dfb4c30a2d6
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-06 14:44:27 -0500

    Have loop generate keys instead of manually writing them

--- src/components/js/Cards.js
+++ src/components/js/Cards.js
@@ -2,7 +2,7 @@ import Card from "./Card.js";
 import "../css/Cards.css";
 
 export default function App() {
-  const cards = getCards();
+  const cards = getJSONData();
 
   // Returns data at the given index and returns true if the index is in range.
   // If index is out of range, returns index 0 which has empty properties, and returns false.
@@ -18,15 +18,14 @@ export default function App() {
     let direction = -150;
     const start = -3, end = 3;
     for (let i = start; i <= end; i++) {
-      let index = i;
-      if (index > 0 && index < cards.length) {
+      if (i > 0 && i < cards.length) {
         cardsArr.push(<Card
-          key={index}
-          index={cards[index].id}   // Index will change when card moves to opposing side of screen. A negative index means it hides its display. It is important to have negative indexes for calculations to work to get new card properties.
+          key={i}
+          index={i}   // Index will change when card moves to opposing side of screen. A negative index means it hides its display. It is important to have negative indexes for calculations to work to get new card properties.
           direction={direction}
-          backgroundColor={cards[index].backgroundColor}
-          frontText={cards[index].frontText}
-          backText={cards[index].backText}
+          backgroundColor={cards[i].backgroundColor}
+          frontText={cards[i].frontText}
+          backText={cards[i].backText}
           getNewData={getNewData}
           amountOfData={cards.length}
         />);
@@ -56,27 +55,24 @@ export default function App() {
   )
 }
 
-function getCards() {
+function getJSONData() {
   return [
     {
-      id: 0,
       backgroundColor: `#`,
       frontText: ` `,
       backText: ``
     },
     {
-      id: 1,
       backgroundColor: `#31587A`,
       frontText: `What are the 4 principles of Object Oriented Programming?`,
       backText: `A PIE.
-      Abstraction
+Abstraction
 Polymorphism
 Inheritance
 Encapsulation
 `
     },
     {
-      id: 2,
       backgroundColor: `#E8525B`,
       frontText: `Explain abstraction`,
       backText: `Abstraction means using simple things to represent complexity. In Java, abstraction
@@ -86,7 +82,6 @@ same work multiple times. It handles complexity by hiding unnecessary details fr
 the user`
     },
     {
-      id: 3,
       backgroundColor: `#E8525B`,
       frontText: `Explain polymorphism`,
       backText: `SAME NAME, MANY FORMS. This Java OOP concept lets programmers use the
@@ -99,7 +94,6 @@ Runtime , Polymorphism handled during runtime: example (Overriding)
 Static , Polymorphism handled in the compiler: example (Overloading)`
     },
     {
-      id: 4,
       backgroundColor: `#31587A`,
       frontText: `Explain inheritance
 `,
@@ -111,7 +105,6 @@ the keyword 'extends'; Members include methods as well as variables.
 `
     },
     {
-      id: 5,
       backgroundColor: `#31587A`,
       frontText: `Explain encapsulation`,
       backText: `This is the practice of keeping fields within a class private, then providing access to
@@ -120,7 +113,6 @@ within the class itself. This way, we can re-use objects like code components or
 variables without allowing open access to the data system-wide.`
     },
     {
-      id: 6,
       backgroundColor: `#31587A`,
       frontText: `What are the 4 access
 modifiers and their access
@@ -131,7 +123,6 @@ Default: Class, Package
 Private: Class, Variable`
     },
     {
-      id: 7,
       backgroundColor: `#31587A`,
       frontText: `What are the 5 SOLID principles?
 `,
@@ -142,25 +133,21 @@ Interface Segregation Principle
 Dependency Inversion`
     },
     {
-      id: 8,
       backgroundColor: `#31587A`,
       frontText: `Explain "Single Responsibility"`,
       backText: `a class should have only a single responsibility (i.e. changes to only one part of the software's specification should be able to affect the specification of the class).`
     },
     {
-      id: 9,
       backgroundColor: `yellow`,
       frontText: `Explain "Open/Close Principle"`,
       backText: `"open for extension, but closed for modification." classes' properties can be inherited and used by subclasses etc, but not altered directly"`
     },
     {
-      id: 10,
       backgroundColor: `#31587A`,
       frontText: `Explain "Liskov's Substitution Principle"`,
       backText: `"objects in a program should be replaceable with instances of their subtypes without altering the correctness of that program."`
     },
     {
-      id: 11,
       backgroundColor: `#31587A`,
       frontText: `Explain "Interface Segregation Principle"`,
       backText: `"many client-specific interfaces are better than one general-purpose interface."
@@ -169,134 +156,112 @@ with empty methods you'll never use, or just the few that offer the functionalit
 are looking for?`
     },
     {
-      id: 12,
       backgroundColor: `#31587A`,
       frontText: `Explain "Dependency Inversion"`,
       backText: `one should "depend upon abstractions, [not] concretions." In other words--don't extend ArrayList class to get those functions; implement List interface instead.`
     },
     {
-      id: 13,
       backgroundColor: `#31587A`,
       frontText: `What is the difference between overloading and overriding?`,
       backText: `Overloading occurs when two or more methods in one class have the same method name but different parameters. Overriding means having two methods with the same method name and parameters (i.e., method signature). One of the methods is in the parent class and the other is in the child class. Overriding changes the BEHAVIOR of the method.`
 
     },
     {
-      id: 14,
       backgroundColor: `#31587A`,
       frontText: `What is the difference between an abstract class and an interface?`,
       backText: `An abstract class can contain non abstract methods and default methods, an interface can only contain method signatures. A class can only extend one abstract class, but it can implement multiple interfaces. An abstract class can have any access modifiers on it's methods and fields, an interface can only be public. Interface fields are public static final. Both can let you use classes of the same supertype.`
     },
     {
-      id: 15,
       backgroundColor: `#31587A`,
       frontText: `For Interface and Abstract Class, why would you use one over the other?`,
       backText: `You can implement multiple interfaces but you can only extend one abstract class. Also, you can flesh out methods more fully in an abstract class; Interfaces are for empty methods.`
     },
     {
-      id: 16,
       backgroundColor: `#31587A`,
       frontText: `What is the differece between "Collection" and "Collections"`,
       backText: `Collections is an utility class present in java.util. package to define several utility method (like sorting,searching ) for collection object. Collections is a class which has some static methods and that method returns the collection. Collection is an interface; root interface in collection hierarchy`
     },
     {
-      id: 17,
       backgroundColor: `#31587A`,
       frontText: `What is an ArrayStoreException? Is it checked or unchecked?`,
       backText: `Thrown to indicate that an attempt has been made to store the wrong type of object into an array of objects. It is an unchecked error that occurs at runtime`
     },
     {
-      id: 18,
       backgroundColor: `#31587A`,
       frontText: `Describe JVM, JDK, and JRE`,
       backText: `JDK: Java development kit is the tool necessary to compile, document and package Java programs. (like SDK) JRE: A subset of the Java Development Kit (JDK) for end-users and developers who want to redistribute the runtime environment alone. The Java runtime environment consists of the Java virtual machine, the Java core classes, and supporting files. JVM: The java virtual machine is a specification that provides run-time environment in which java byte code can be executed.`
     },
     {
-      id: 19,
       backgroundColor: `yellow`,
       frontText: `What is a HashMap?`,
       backText: `HashMap is a Map based collection class that is used for storing Key & value pairs, does not sort the stored keys and Values TALK ABOUT BUCKETS AND COLLISION`,
     },
     {
-      id: 20,
       backgroundColor: `#31587A`,
       frontText: `Why is a HashMap unsorted?`,
       backText: `Two reasons: One, it IS sorted by Java's own internal logic; entries are sorted by hashcode. It is not sorted in any human-readable way Two, HashMaps are SETS of key-value pairs, and sets are (often) unsorted sets of unique values.`
     },
     {
-      id: 21,
       backgroundColor: `#31587A`,
       frontText: `What is the difference between Comparable and Comparator?`,
       backText: `Comparable: This interface has one method, compareTo(). Class with objects to be sorted must implement this Comparable interface. Comparator: This interface has two methods, equals() and compare(). Class with objects to be sorted do not need to implement this Comparator interface.`
     },
     {
-      id: 22,
       backgroundColor: `#31587A`,
       frontText: `What is Class Loader?`,
       backText: `Part of JVM which is used to load classes and interfaces.`
     },
     {
-      id: 23,
       backgroundColor: `#31587A`,
       frontText: `What are the 5 exception keywords? `,
       backText: `Try, Catch, Finally, Throw, Throws`
     },
     {
-      id: 24,
       backgroundColor: `#31587A`,
       frontText: `Difference between throw and throws?`,
       backText: `"Throws" goes in signature: void method() throws Exception {} "Throw" goes in method body: void method() { throw new Exception; }`
     },
     {
-      id: 25,
       backgroundColor: `#31587A`,
       frontText: `Difference between Final, Finally, Finalize?`,
       backText: `Final: Final is a keyword used to apply restrictions on class, method and variable. Final class can't be inherited, final method can't be overridden and final variable value can't be changed. Finally: Finally is a code block used to place important code, it will be executed whether exception is handled or not. Finalize: Finalize is a method used to perform clean up processing just before object is garbage collected`
     },
     {
-      id: 26,
       backgroundColor: `#31587A`,
       frontText: `What is Dictionary in Java?`,
       backText: `The Dictionary class is the abstract parent of any class, such as Hashtable , which maps keys to values. Any non- null object can be used as a key and as a value.`
     },
     {
-      id: 27,
       backgroundColor: `#31587A`,
       frontText: `Which collection is the fastest?`,
       backText: `In case of index value pair- arraylist, value- hashset and key value pair- hashmap`
     },
     {
-      id: 28,
       backgroundColor: `#31587A`,
       frontText: `What is an instance variable?`,
       backText: `An attribute, or field of an object.`
     },
     {
-      id: 29,
       backgroundColor: `#31587A`,
       frontText: `What is the finalize method?`,
       backText: `The method called before garbage collection on any Java object.`
     },
     {
-      id: 30,
       backgroundColor: `#31587A`,
       frontText: `What is an access modifier?`,
       backText: `A keyword that describes who or what can access or modifiy the state of an object. Examples: public, private, default, protected`
     },
     {
-      id: 31,
       backgroundColor: `#31587A`,
       frontText: `What is an abstract class?`,
       backText: `A class that contains one or more abstract methods, and therefore can never be instantiated. Abstract classes are defined so that other classes can extend them and make them concrete by implementing the abstract methods.`
     },
     {
-      id: 32,
       backgroundColor: `#31587A`,
       frontText: `What is an abstract method?`,
       backText: `A method that has no implementation. ex interface methods are abstract`
     },
     {
-      id: 33,
       backgroundColor: `#31587A`,
       frontText: `What is an API?`,
       backText: `Application Programming Interface. The specification of how a programmer writing an application accesses the behavior and state of classes and objects.`
