6f28ba70a94271084aac802ce90e8d55ffe0e525
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-07 01:14:25 -0500

    Add directions

--- src/App.js
+++ src/App.js
@@ -6,6 +6,12 @@ function App() {
     <div className="App">
       <h1 className="title App-logo">Java</h1>
       <Cards></Cards>
+      <div className="controls-description">
+        <p>Use the arrow keys to cycle</p>
+        <p>Space bar to flip</p>
+        <p>Up key if you knew the answer</p>
+        <p>And click a card to edit</p>
+      </div>
     </div>
   );
 }
