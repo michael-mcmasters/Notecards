763474ab687a2b48dfc4141d6746db71e7a07eb2
michael-mcmasters <jmcmasters411@gmail.com>
2021-02-07 00:33:23 -0500

    Create card-bump-up animation

--- src/components/js/Card.js
+++ src/components/js/Card.js
@@ -101,6 +101,7 @@ export default function Card(props) {
     setBackgroundColor(newProperties.backgroundColor);
     setFrontText(newProperties.frontText);
     setBackText(newProperties.backText);
+    setBackTextBeforeEdit(newProperties.backText);
     if (indexInRange === false) {
       setDisplay("none");
     } else {
@@ -184,10 +185,7 @@ export default function Card(props) {
 };
 
 const cardBumpUpAnimation = keyframes`
-   0% { height: 100px; width: 100px; }
-  30% { height: 400px; width: 400px; opacity: 1 }
-  40% { height: 405px; width: 405px; opacity: 0.3; }
-  100% { height: 100px; width: 100px; opacity: 0.6; }
+   50% { margin-top: 1rem; }
 `;
 
 // left moves card into position, pushing it away from the left side of the screen.
@@ -209,7 +207,7 @@ const FlipCardContainer = styled.div`
     
     /* animation-name: ${cardBumpUpAnimation}; */
     animation-name: ${props => props.cardAcceped ? cardBumpUpAnimation : ""};
-    animation-duration: 8s;
+    animation-duration: 0.4s;
     /* animation-iteration-count: infinite; */
 `;
 
