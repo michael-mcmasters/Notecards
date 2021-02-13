7bc08b44c05614950ab6b25b0fea205925a5423b
Michael McMasters <jmcmasters411@gmail.com>
2021-02-12 23:57:13 -0500

    Create setup for object pooling and useReducer

--- src/components/js/CardReducer.js
+++ src/components/js/CardReducer.js
@@ -3,7 +3,8 @@ import React from 'react';
 const CardReducer = () => {
   return (
     <div>
-
+      <p>Hello</p>
+      <p>{console.log("hello")}</p>
     </div>
   );
 };
--- src/components/js/CardsReducer.js
+++ src/components/js/CardsReducer.js
@@ -2,11 +2,22 @@ import React, { useReducer } from 'react';
 import CardReducer from "./CardReducer";
 import cardsJSON from "../../resources/card-data.json";
 
+// function reducer(state, action) {
+//   switch (action.type) {
+//     case "get-next-index":
+//       return { cards: state.cards, numOfCards: state.numOfCards }
+//     case "increment-num-of-cards":
+//       return { cards: state.cards, numOfCards: state.numOfCards + 1 }
+//     default:
+//       return state;
+//   }
+// }
+
 function reducer(state, action) {
   switch (action.type) {
-    case "get-next-index":
+    case "cycle-left":
       return { cards: state.cards, numOfCards: state.numOfCards }
-    case "increment-num-of-cards":
+    case "cycle-right":
       return { cards: state.cards, numOfCards: state.numOfCards + 1 }
     default:
       return state;
@@ -15,18 +26,64 @@ function reducer(state, action) {
 
 const CardsReducer = () => {
   const cards = cardsJSON.cards;
-  const [state, dispatch] = useReducer(reducer, { cards: cards, numOfCards: 1 })
+
+  let [cardContainers, dispatch] = useReducer(reducer, [
+    {
+      cardIndex: -3,
+      xPosition: "-150",
+      flipped: false,
+    },
+    {
+      cardIndex: -2,
+      xPosition: "-100",
+      flipped: false,
+    },
+    {
+      cardIndex: -1,
+      xPosition: "-50",
+      flipped: false,
+    },
+    {
+      cardIndex: 0,
+      xPosition: "0",
+      flipped: false,
+    },
+    {
+      cardIndex: 1,
+      xPosition: "50",
+      flipped: false,
+    },
+    {
+      cardIndex: 2,
+      xPosition: "100",
+      flipped: false,
+    },
+    {
+      cardIndex: 3,
+      xPosition: "150",
+      flipped: false,
+    },
+  ]);
 
   function handleClick() {
-    dispatch({ type: "increment-num-of-cards" })
+    // dispatch({ type: "cycle-left" })
   }
 
+
   return (
-    <div>
-      <CardReducer></CardReducer>
-      <p>{state.numOfCards}</p>
+    <>
+      {cardContainers.map(c => {
+        if (c.cardIndex < 0 || c.cardIndex >= cards.length) c.cardIndex = 0;
+        return <CardReducer
+          card={c.card}
+          cardIndex={c.index}
+          xPosition={c.xPosition}
+          flipped={c.flipped}
+        />
+      })}
+
       <button style={{ padding: "4em" }} onClick={handleClick}></button>
-    </div>
+    </>
   );
 };
 
