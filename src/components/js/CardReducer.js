fbe351675c2f4fe5113bca5969b8da7e884ffb90
Michael McMasters <jmcmasters411@gmail.com>
2021-02-13 22:58:35 -0500

    Implement styled component

--- src/components/js/CardReducer.js
+++ src/components/js/CardReducer.js
@@ -1,13 +1,24 @@
 import React from 'react';
+import styled, { keyframes } from "styled-components";
 
 const CardReducer = ({ card, cardIndex, xPosition, flipped }) => {
   return (
-    <div style={{ width: "300px", border: "1px solid gray" }}>
+    <Cardy xPosition={xPosition}>
       <p>{xPosition}</p>
       <p>{cardIndex}</p>
       <p>{card.backText}</p>
-    </div>
+    </Cardy>
   );
 };
+
+const Cardy = styled.div`
+  position: absolute;
+  width: 300px;
+  border: 1px solid gray;
+  transition: 0.39s ease;
+  left: ${props => props.xPosition}%;
+  transform: translateX(-50%);
+  -webkit-transform:translateX(-50%);
+`;
 
 export default CardReducer;
