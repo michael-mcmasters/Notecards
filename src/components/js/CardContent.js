9a60baeb0c2c99d591cd867cf61320694b84607f
Michael McMasters <jmcmasters411@gmail.com>
2021-02-20 21:12:11 -0500

    Text on cards are now input fields

--- src/components/js/CardContent.js
+++ src/components/js/CardContent.js
@@ -5,7 +5,9 @@ const CardContent = ({ text }) => {
   return (
     <div>
       <Container>
-        {text}
+        <Input>
+          {text}
+        </Input>
       </Container>
     </div>
   );
@@ -13,7 +15,55 @@ const CardContent = ({ text }) => {
 
 const Container = styled.div`
   padding: 1rem;
-  font-size: 30px;
+`;
+
+// Have to set height manually because textarea's don't have an auto height property, and they don't fill parent div's height.
+const Input = styled.textarea`
+font-size: 30px;
+  height: 240px;
+  width: 100%;
+  background-color: transparent;
+  border: none;
+  resize: none;
+`;
+
+const CancelButton = styled.button`
+  position: relative;
+  border: none;
+  width: 4em;
+  height: 4em;
+  margin-bottom: 3em;
+  margin-right: 1em;
+  border-radius: 100px;
+  background-color: ${props => props.pressed ? "green" : "red"};
+  
+  bottom: ${props => props.userEditingText ? "-5em" : "1em"};
+  transition: bottom 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition-delay: ${props => props.userEditingText ? "0.2s" : "0"};
+  cursor: pointer;
+  
+  &:focus {
+    outline: none;
+  }
+`;
+
+const SaveButton = styled.button`
+  position: relative;
+  border: none;
+  width: 4em;
+  height: 4em;
+  margin-bottom: 3em;
+  border-radius: 100px;
+  background-color: ${props => props.pressed ? "green" : "red"};
+  
+  bottom: ${props => props.userEditingText ? "-5em" : "1em"};
+  transition: bottom 0.4s cubic-bezier(0.175, 0.885, 0.32, 1.275);
+  transition-delay: ${props => props.userEditingText ? "0.5s" : "0"};
+  cursor: pointer;
+  
+  &:focus {
+    outline: none;
+  }
 `;
 
 export default CardContent;
