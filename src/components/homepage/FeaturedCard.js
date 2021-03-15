012fab32cc898fd360dac3e636c514556729199c
Michael McMasters <jmcmasters411@gmail.com>
2021-03-15 02:33:42 -0400

    Add footer element with margin

--- src/components/homepage/FeaturedCard.js
+++ src/components/homepage/FeaturedCard.js
@@ -4,20 +4,24 @@ import styled from "styled-components";
 const FeaturedCard = () => {
   const [revealedAnswer, setRevealedAnswer] = useState(false);
 
-
+  const handleRevealAnswer = () => {
+    setRevealedAnswer(true);
+  }
 
   return (
     <Container>
       <Header>
         Featured Card!
       </Header>
-      <Card>What are the 4 access modifiers and their access specifiers</Card>
+      <Card flipped={revealedAnswer}>What are the 4 access modifiers and their access specifiers</Card>
 
-      {!revealedAnswer
-        ? <Button onClick={() => setRevealedAnswer(true)}>Reveal Answer</Button>
+      <Footer>
+        {!revealedAnswer
+          ? <Button onClick={handleRevealAnswer}>Reveal Answer</Button>
 
-        : "Did you guess correctly?"
-      }
+          : "Did you guess correctly?"
+        }
+      </Footer>
     </Container>
   );
 };
@@ -33,11 +37,16 @@ const Header = styled.h1`
 `;
 
 const Card = styled.div`
-  background-color: aqua;
+  /* background-color: aqua; */
   padding: 1rem;
   border-radius: 10px;
   width: 20rem;
   height: 10rem;
+  background-color: ${props => props.flipped ? "green" : "aqua"};
+`;
+
+const Footer = styled.div`
+  margin-top: 1rem;
 `;
 
 const Button = styled.button`
