203966f34c87ec044eeb388cbf33224e337fa031
Michael McMasters <jmcmasters411@gmail.com>
2021-03-15 02:56:58 -0400

    Add Card component to FeaturedCards

---
+++ src/components/homepage/Card.js
@@ -0,0 +1,101 @@
+import React from 'react';
+import styled, { css } from "styled-components";
+
+const Card = ({ card, flipped }) => {
+  return (
+    <Container>
+      <FlipCard flipped={flipped}>
+        <Front backgroundColor={card.backgroundColor} scrollbarColor="white">
+          Front
+        </Front>
+        <Back scrollbarColor={card.backgroundColor}>
+          Back
+        </Back>
+      </FlipCard>
+    </Container>
+  );
+};
+
+const Container = styled.div`
+  display: ${props => props.display};
+  width: 450px;
+  height: 320px;
+  background: none;
+  margin: 0;
+  margin-top: 2rem;
+  cursor: pointer;
+  position: absolute;
+  transition: ${props => props.transition};
+
+  left: ${props => props.xPosition}%;
+  transform: translateX(-50%);
+  -webkit-transform:translateX(-50%);
+  
+  animation-duration: 0.4s;
+`;
+
+const FlipCard = styled.div`
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  transform-style: preserve-3d;
+  transition: all 0.8s ease;
+  transform: ${props => props.flipped ? "rotateY(180deg)" : "rotateY(0deg)"};
+`;
+
+const ScrollBar = css`
+    textarea::-webkit-scrollbar {
+    color: transparent;
+    background-color: transparent;
+    width: 10px;      // Width of verticle scrollbar
+    height: 0px;      // 0px hides the horizontal scrollbar
+  }
+
+  textarea::-webkit-scrollbar-thumb:vertical{
+    background: ${props => props.scrollbarColor};
+    border-radius: 10px;
+  } 
+  
+  textarea::-webkit-scrollbar-thumb::horizontal{
+    height: 0px;
+  }
+  
+  textarea::-webkit-scrollbar-corner {
+    display: none;
+  }
+`;
+
+const Front = styled.div`
+  font-size: 28px;
+
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  backface-visibility: hidden;
+  -webkit-backface-visibility: hidden;
+  overflow: hidden;
+
+  background-color: ${props => props.backgroundColor}; 
+  
+  ${ScrollBar}
+`;
+
+const Back = styled.div`
+  font-size: ${props => props.fontSize};
+  position: absolute;
+  top: 0;
+  left: 0;
+  width: 100%;
+  height: 100%;
+  border-radius: 10px;
+  backface-visibility: hidden;
+  overflow: hidden;
+  background: #fafafa;
+  color: #333;
+  text-align: center;
+  transform: rotateY(180deg);
+  
+  ${ScrollBar}
+`;
+
+export default Card;
--- src/components/homepage/FeaturedCard.js
+++ src/components/homepage/FeaturedCard.js
@@ -1,7 +1,10 @@
 import React, { useState } from 'react';
 import styled from "styled-components";
+import cardsJSON from "../../resources/card-data.json";
+import Card from "./Card.js";
 
 const FeaturedCard = () => {
+  const [cards, setCards] = useState(cardsJSON.cards);
   const [revealedAnswer, setRevealedAnswer] = useState(false);
 
   const handleRevealAnswer = () => {
@@ -17,7 +20,8 @@ const FeaturedCard = () => {
       <Header>
         Featured Card!
       </Header>
-      <Card flipped={revealedAnswer}>What are the 4 access modifiers and their access specifiers</Card>
+      {/* <Card flipped={revealedAnswer}>What are the 4 access modifiers and their access specifiers</Card> */}
+      <Card card={cards[0]} flipped={revealedAnswer} />
 
       <Footer>
         {!revealedAnswer
@@ -45,14 +49,14 @@ const Header = styled.h1`
   
 `;
 
-const Card = styled.div`
-  /* background-color: aqua; */
-  padding: 1rem;
-  border-radius: 10px;
-  width: 20rem;
-  height: 10rem;
-  background-color: ${props => props.flipped ? "green" : "aqua"};
-`;
+// const Card = styled.div`
+//   /* background-color: aqua; */
+//   padding: 1rem;
+//   border-radius: 10px;
+//   width: 20rem;
+//   height: 10rem;
+//   background-color: ${props => props.flipped ? "green" : "aqua"};
+// `;
 
 const Footer = styled.div`
   margin-top: 1rem;
