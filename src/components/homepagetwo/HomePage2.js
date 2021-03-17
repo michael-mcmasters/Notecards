ee9f796e98e883aa611a4fcbe8465fd0e9ae7198
Michael McMasters <jmcmasters411@gmail.com>
2021-03-16 22:23:01 -0400

    Make background cover entire viewport height when content is small

--- src/components/homepagetwo/HomePage2.js
+++ src/components/homepagetwo/HomePage2.js
@@ -4,21 +4,20 @@ import { GiCardRandom } from "react-icons/gi";
 
 const HomePage2 = () => {
   return (
-    <Background>
+    <Container>
       <Icon>
         <GiCardRandom />
       </Icon>
-    </Background>
+    </Container>
   );
 };
 
-const Background = styled.div`
-  background-image: linear-gradient(to top, #DB7092 20%, #D9A059 80%);
-  height: 100vh;
+const Container = styled.div`
+  margin-top: 10rem;
+  display: flex;
 `;
 
 const Icon = styled(GiCardRandom)`
-  margin-top: 10rem;
   color: white;
   height: 10rem;
   width: 10rem;
