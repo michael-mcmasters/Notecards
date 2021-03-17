89b0facdec8f46a2277cebb8fb2f3ada10303a06
Michael McMasters <jmcmasters411@gmail.com>
2021-03-16 22:47:49 -0400

    Place buttons beside icon

--- src/components/homepagetwo/HomePage2.js
+++ src/components/homepagetwo/HomePage2.js
@@ -4,23 +4,48 @@ import { GiCardRandom } from "react-icons/gi";
 
 const HomePage2 = () => {
   return (
-    <Container>
+    <FlexContainer>
       <Icon>
         <GiCardRandom />
       </Icon>
-    </Container>
+      <ButtonContainer>
+        <Button>View Decks</Button>
+        <Button>Create Deck</Button>
+      </ButtonContainer>
+    </FlexContainer>
   );
 };
 
-const Container = styled.div`
+const FlexContainer = styled.div`
   margin-top: 10rem;
   display: flex;
+  justify-content: center;
+  align-items: center;
 `;
 
 const Icon = styled(GiCardRandom)`
   color: white;
-  height: 10rem;
-  width: 10rem;
+  height: 13rem;
+  width: 13rem;
+  margin-right: 6em;
+`;
+
+const ButtonContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  justify-content: space-evenly;
+`;
+
+const Button = styled.button`
+  padding: 1em 1em;
+  margin: 0.5rem;
+  background-color: #1F2C76;
+  font-weight: bold;
+  font-size: 1.2rem;
+  color: white;
+  border-radius: 10px;
+  border: none;
+  
 `;
 
 export default HomePage2;
